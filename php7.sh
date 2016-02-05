cb01932e383fe82c8f8e3f8af5fd8ca230d8e137
Jerry 张翊奇 <zhangyiqi@gdsiyue.com>
2016-02-05 16:59:21 +0800

    Updated download from Dockerfile
    Signed-off-by: Jerry 张翊奇 <zhangyiqi@gdsiyue.com>

--- php7.sh
+++ php7.sh
@@ -4,26 +4,12 @@ DIR_NAME=$1
 REPOSITORY_ADDR=$2
 DOCKER_IMAGE=$3
 DOCKER_CONTAINER=$4
-PHP_VERSION=$5
-LOG_PATH=$6
-APP_CONTAINER=$7
-DB_CONTAINER=$8
-CACHE_CONTAINER=$9
-PHP_TAR_NAME="${PHP_VERSION}.tar.gz"
-PHP_TAR_DIR="${DIR_NAME}/software"
-PHP_TAR_FILE="${PHP_TAR_DIR}/${PHP_TAR_NAME}"
-PHP_TAR_FINAL_FILE="${PHP_TAR_DIR}/php.tar.gz"
-DOWNLOAD_URL="http://cn2.php.net/get/${PHP_TAR_NAME}/from/this/mirror"
+LOG_PATH=$5
+APP_CONTAINER=$6
+DB_CONTAINER=$7
+CACHE_CONTAINER=$8
 
 ./updateRepository.sh ${DIR_NAME} ${REPOSITORY_ADDR}
-
-if [ ! -d ${PHP_TAR_DIR} ]; then
-  mkdir -p $PWD/${PHP_TAR_DIR}
-fi
-
-if [ ! -f ${PHP_TAR_FINAL_FILE} ]; then
-  wget ${DOWNLOAD_URL} -O ${PHP_TAR_FINAL_FILE}
-fi 
 
 if [ ! -d ${LOG_PATH} ]; then
   sudo mkdir -p ${LOG_PATH}
